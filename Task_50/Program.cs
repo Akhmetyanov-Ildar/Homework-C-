4462f9eb134828a1a276aa432a23db1d2ed1bb81
Ildar <example.ru>
2022-07-16 21:28:48 +0500

    Task50 done

---
+++ Task_50/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+Console.Clear();
+
+int[,] CreateMatrixRndInt(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+void PrintElementByIndex(int row, int column)
+{
+    int[,] arr = CreateMatrixRndInt(5, 5);
+    PrintMatrix(arr);
+    if (row > arr.GetLength(0) - 1 || column > arr.GetLength(1) - 1)
+    {
+        Console.WriteLine("такого числа в массиве нет");
+    }
+    else
+    {
+        Console.WriteLine(arr[row, column]);
+    }
+
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3} | ");
+            else Console.Write($"{arr[i, j],3} | ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите первый индекс: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второй индекс: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+PrintElementByIndex(row, column);
